e9ba3b8cc79183dcf6a357ba6896b7d343b6dca6
Vince Bmmrt <vincebommert75@gmail.com>
2023-11-13 16:41:49 +0100

    récup data depuis redux + Message Props

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,8 +1,13 @@
-function Message() {
+type MessageProps = {
+  author: string;
+  content: string;
+};
+
+function Message({ author, content }: MessageProps) {
   return (
     <div className="message">
-      <div className="message__author">author</div>
-      <div className="message_content">message content</div>
+      <div className="message__author">{author}</div>
+      <div className="message_content"> {content}</div>
     </div>
   );
 }
--- src/components/MessageList/MessageList.tsx
+++ src/components/MessageList/MessageList.tsx
@@ -1,12 +1,20 @@
-import { v4 as uuidv4 } from 'uuid';
+import { useAppSelector } from '../../hooks/redux';
 import Message from '../Message/Message';
 import './MessageList.scss';
 
 function MessageList() {
+  // Depuis mon store redux, je vais récupérer la liste des messages
+  // Je récupère la donnée ET je m'abonne à ses modifications
+  const messages = useAppSelector((state) => state.chat.messages);
+
   return (
     <div className="message-list">
-      {Array.from({ length: 50 }).map(() => (
-        <Message key={uuidv4()} />
+      {messages.map((message) => (
+        <Message
+          key={message.id}
+          author={message.author}
+          content={message.content}
+        />
       ))}
     </div>
   );
