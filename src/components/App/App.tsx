1acb2df2c8dac111d050111616c65e0a5d3deb0c
Vince Bmmrt <vincebommert75@gmail.com>
2023-11-13 21:58:13 +0100

    Settingscomponent

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,19 @@
 import Form from '../Form/Form';
 
 import MessageList from '../MessageList/MessageList';
+import Settings from '../Settings/Settings';
 
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <MessageList />
-      <Form />
-    </div>
+    <>
+      <Settings />
+      <div className="App">
+        <MessageList />
+        <Form />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Settings/Settings.tsx
@@ -0,0 +1,21 @@
+import './Settings.scss';
+
+function Settings() {
+  return (
+    <div className="settings">
+      Settings
+      <button type="button" className="settings__toggle">
+        X
+      </button>
+      <form className="settings__form">
+        <input type="email" className="settings__input" />
+        <input type="password" className="settings__input" />
+        <button type="submit" className="settings__submit">
+          Envoyer
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Settings;
