282de2e4dff053248893a67f747e6b4c445d5311
Vince Bmmrt <vincebommert75@gmail.com>
2023-11-13 21:42:51 +0100

    scroll en bas a chaque messages

--- src/components/MessageList/MessageList.tsx
+++ src/components/MessageList/MessageList.tsx
@@ -1,11 +1,23 @@
+import { useEffect, useRef } from 'react';
 import { useAppSelector } from '../../hooks/redux';
 import Message from '../Message/Message';
 import './MessageList.scss';
 
 function MessageList() {
+  // Je créer une nouvelle ref, je précise avec TypeScript qu'elle contiendra un element HTMLDivElement
+  // On doit l'initialiser à null
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+
   // Depuis mon store redux, je vais récupérer la liste des messages
   // Je récupère la donnée ET je m'abonne à ses modifications
   const messages = useAppSelector((state) => state.chat.messages);
+  useEffect(() => {
+    // scrollIntoView permet de scroller jusqu'à un élément
+    // Comme mon élément est en bas de ma liste de message. Je scroll en bas de la liste de message.
+    // J'utilise l'élément stocker dans ma ref pour scroller jusqu'à lui
+    // Le `?` est mis avis d'éviter les erreurs si mon élément HTML n'est pas trouvé
+    messagesEndRef.current?.scrollIntoView();
+  }, [messages]);
 
   return (
     <div className="message-list">
@@ -16,6 +28,7 @@ function MessageList() {
           content={message.content}
         />
       ))}
+      <div ref={messagesEndRef} />
     </div>
   );
 }
