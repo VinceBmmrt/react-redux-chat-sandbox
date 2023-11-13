fb08ab71b461a9270adaa69c757cbdf82ac2dc2c
Vince Bmmrt <vincebommert75@gmail.com>
2023-11-13 16:06:43 +0100

    mock data dans initialState

--- src/store/reducers/chat.ts
+++ src/store/reducers/chat.ts
@@ -1,6 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {};
+type Message = {
+  id: string;
+  author: string;
+  content: string;
+};
+
+type ChatState = {
+  messages: Message[];
+};
+
+const initialState: ChatState = {
+  messages: [
+    { id: crypto.randomUUID(), author: 'moi', content: 'hello ca va ?' },
+    { id: crypto.randomUUID(), author: 'moundir', content: 'BOF' },
+    {
+      id: crypto.randomUUID(),
+      author: 'Denis',
+      content: 'HA !',
+    },
+    { id: crypto.randomUUID(), author: 'moi', content: 'ok' },
+  ],
+};
 
 const chatSlice = createSlice({
   name: 'chat',
